3eaefae52c1739b305665d3ee6ebc96a4a49b4c6
CM <etchiedanie1@gmail.com>
2022-11-14 07:41:35 +0100

    style: style the header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,8 @@
 import classes from "./Header.module.css";
+import house_1 from "../../images/house_1.png"
+import house_2 from "../../images/house_2.png"
+import house_3 from "../../images/house_3.png"
+import house_4 from "../../images/house_4.png"
 
 const Header = () => {
   return (
@@ -10,16 +14,24 @@ const Header = () => {
           <span className={classes.highlight__bold}>Metaverse</span>
         </h1>
         <p className={classes.main__content__p}>
-          we provide you access to luxury and affordable houses <br /> in the
-          metaverse, get a chance to turn your <br /> imagination to reality at your
+          we provide you access to luxury and affordable houses in the
+          metaverse, get a chance to turn your imagination to reality at your
           comfort zone
         </p>
+        <div className={classes.search}>
+          <input className={classes.search__input} placeholder="Search for location" />
+          <button className={classes.search__btn}>Search</button>
+        </div>
       </section>
 
       <section
         arial-aria-label="image__content"
-        className={classes.img__section}
-      ></section>
+        className={classes.img__section}>
+          <img src={house_1} className={classes.header__img} alt="savana house" />
+          <img src={house_2} className={classes.header__img} alt="gucci house" />
+          <img src={house_3} className={classes.header__img} alt="special house" />
+          <img src={house_4} className={classes.header__img} alt="test house" />
+      </section>
     </header>
   );
 };
