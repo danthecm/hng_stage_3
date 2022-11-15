aa2cbd1fd45c5d8902fac3a3ae08a22410c127c5
CM <etchiedanie1@gmail.com>
2022-11-15 22:27:53 +0100

    style: made all pages and components fully mobile responsive

--- src/common/Navbar/Navbar.js
+++ src/common/Navbar/Navbar.js
@@ -5,6 +5,7 @@ import { useState } from "react";
 
 const Navbar = (props) => {
   const [showModal, setShowModal] = useState(false);
+  const [showNav, setShowNav] = useState(false);
 
   const connectBtnHandler = () => {
     setShowModal(true);
@@ -13,24 +14,43 @@ const Navbar = (props) => {
   const clearModal = () => {
     setShowModal(false);
   };
+
+  const toggleNavHandler = () => {
+    setShowNav((state) => !state);
+  };
+
   return (
     <>
       <Modal showModal={showModal} clearModal={clearModal} />
       <nav aria-label="Main" className={classes.navbar}>
-        <img src={logo} alt="Metabnb Logo" />
-        <ul className={classes.nav__items}>
-          <li className={classes.nav__item}>
-            <a href="/">Home</a>
-          </li>
-          <li className={classes.nav__item}>
-            <a href="/place2stay">Place to stay</a>
-          </li>
-          <li className={classes.nav__item}>NFTs</li>
-          <li className={classes.nav__item}>Community</li>
-        </ul>
-        <button onClick={connectBtnHandler} className={classes.btn__connect}>
-          Connect wallet
-        </button>
+        <div className={classes.hanger_nav} onClick={toggleNavHandler}>
+          <img src={logo} alt="Metabnb Logo" />
+          <div className={classes.hanger}>
+            <div className={classes.buger}></div>
+            <div className={classes.buger}></div>
+            <div className={classes.buger}></div>
+          </div>
+        </div>
+        {showNav && (
+          <>
+            <ul className={classes.nav__items}>
+              <li className={classes.nav__item}>
+                <a href="/">Home</a>
+              </li>
+              <li className={classes.nav__item}>
+                <a href="/place2stay">Place to stay</a>
+              </li>
+              <li className={classes.nav__item}>NFTs</li>
+              <li className={classes.nav__item}>Community</li>
+            </ul>
+            <button
+              onClick={connectBtnHandler}
+              className={classes.btn__connect}
+            >
+              Connect wallet
+            </button>{" "}
+          </>
+        )}
       </nav>
     </>
   );
